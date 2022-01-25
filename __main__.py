345c2f9a337d1177e0275eb6ad082558095a6a66
Rezende123 <feliperezende.srz@gmail.com>
2022-01-25 19:59:56 -0300

    feat: Escuta o teclado ao invés de definir uma entrada no terminal

--- __main__.py
+++ __main__.py
@@ -1,15 +1,18 @@
-from services.control import controlMouse
+from pynput import keyboard
+from services.control import controlMouseWithKeys
+
+def on_press_key(tecla):
+    try:
+        # print("Tecla pressionada: {0}".format(tecla))
+        controlMouseWithKeys(tecla)
+    except AttributeError:
+        print("Tecla especial pressionada: {0}".format(tecla))
 
 def main():
     print("Para controlar o mouse use as teclas W(cima) A(esquerda) S(direita) D(baixo)\n")
-
-    tecla = input().upper()
     
-    while tecla == "W" or tecla == "A" or tecla == "S" or tecla == "D" :
-        controlMouse(tecla)
-        tecla = input().upper()
-
-    print("FIM!!")
+    with keyboard.Listener(on_press=on_press_key) as listener:
+        listener.join()
 
 main()
 
--- services/control.py
+++ services/control.py
@@ -1,15 +1,16 @@
+from pynput import keyboard
 from models.mouse import Mouse
 
-defTargetMove = 10
 
-def controlMouse(tecla):
+def controlMouseWithKeys(tecla):
+    defTargetMove = 10
     mouse = Mouse()
     
-    if tecla == "W":
+    if tecla == keyboard.KeyCode.from_char('w'):
         mouse.move(0, -defTargetMove)
-    elif tecla == "A":
+    elif tecla == keyboard.KeyCode.from_char('a'):
         mouse.move(-defTargetMove, 0)
-    elif tecla == "D":
+    elif tecla == keyboard.KeyCode.from_char('d'):
         mouse.move(defTargetMove, 0)
-    elif tecla == "S":
+    elif tecla == keyboard.KeyCode.from_char('s'):
         mouse.move(0, defTargetMove)
